b4d98fa20f01922724b631fe939be8e3c64071ed
Phuong Dang <powoftech@gmail.com>
2024-06-13 01:40:45 +0700

    chore: Update logging and server startup messages

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ import env from "./utils/cleanEnv.js";
 const port = env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log(`app listening on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port} 🚀`);
+  console.log(`Press CTRL+C to stop it`);
 });
